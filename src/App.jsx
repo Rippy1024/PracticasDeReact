ed4949605b06fe380b019cbc71dafafa7c23dec0
Nano <luxorius07@gmail.com>
2024-02-04 15:49:49 -0300

    agregamos 2 props en TituloSecundario

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import TituloSecundario from "./components/TituloSecundario";
 
 function App() {
   //aquí agregamos toda la lógica que necesite el componente
-
+const anioActual = 2024;
   return (
     //aquí puedo agregar un poquito mas de lógica si necesito, después iría un <></> fragment
     <>
       {/* aqui empiezo a maquetar comentario de html */}
       <section className="container my-4">
         <h1 className="display-3 text-center text-danger">Contador con React</h1>
-        <TituloSecundario/>
+        <TituloSecundario comision='c73i' anioActualProps={anioActual} />
       </section>
     </>
   );
